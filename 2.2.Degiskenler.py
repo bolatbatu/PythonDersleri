1b17f8b218326c95808640f8468210328726710c
Batuhan <batuhanbolat3@gmail.com>
2023-02-19 15:13:39 +0300

    degisiklik

--- 2.2.Degiskenler.py
+++ 2.2.Degiskenler.py
@@ -1,1 +1,3 @@
-print(bool("0"))
+print(bool(0))         #False   Eğer 1 olursa True olur. İnt ifadelerinde 1 ya da 0' a bakmaktadır.
+print(bool(""))        #False   Str ifadelerinde içinin dolu olup olmadığına bakar.
+print(bool("Hello"))   #True    İçi dolu ise True boş ise False olur.
