0d5da03aa4cbdbfdc3905bbc506adb5fdb597aa3
Batuhan <batuhanbolat3@gmail.com>
2023-02-26 14:39:39 +0300

    güncelleme

--- Örnekler/5.Ornek_notortalamasihesaplama.py
+++ Örnekler/5.Ornek_notortalamasihesaplama.py
@@ -3,18 +3,14 @@
 #2*notlarin ortalamasi alınacak                                                yapıldı.
 #3*notların geçip geçmediğini göreceğiz.                                       yapılmadı.
 
-sayi = input("Virgül ile sayıları giriniz.: ")
+sayi = input("Virgül ile sayilari giriniz.: ")
  
-print("Girdiğiniz Sayılar: {0}".format(sayi))
+print("Girdiginiz Sayilar: {0}".format(sayi))
  
 ortalama=sayi.split(",")                                                                #split(",") komutu ile virgülleri sileriz.
 toplam = 0 
 for n in ortalama:
    toplam = toplam + int(n)                                                             #sayilarin toplamini hesaplar.
  
-print("GİRDİĞİNİZ SAYİLARİN NOT ORTALAMASI:{0:.2f} ".format(toplam / len(ortalama)))    #len(ortalama) ile kaç adet sayi olduğunu hesaplariz.
+print("GIRDIGINIZ SAYILARIN NOT ORTALAMASI:{0:.2f} ".format(toplam / len(ortalama)))    #len(ortalama) ile kaç adet sayi olduğunu hesaplariz.
 #format(toplam / len(ortalama)) ile sayilarin toplamini sayi adetine bölerek ortalamayi hesaplariz.
-#if ortalama != (toplam / len(ortalama)):
-    #print("Kaldınız.",sayi)
-#if ortalama > (toplam / len(ortalama)):
-    #print("Geçtiniz.",sayi)
