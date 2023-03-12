4b79f10f0aeba15e32735f35a7515d65094d5bbf
Batuhan <batuhanbolat3@gmail.com>
2023-03-12 17:10:10 +0300

    yeni

---
+++ 17.5.Fonksiyon.py
@@ -0,0 +1,18 @@
+#----Faktoriyel Fonksiyonu----
+def factorial(n):
+    if n==1:
+        return 1
+    else:
+        return n*factorial(n-1)
+print(factorial(5)) # 5*4*3*2*1 = 120 
+
+#-----Fibonacci Fonksiyonu----
+def Fibonacci(n):
+    if n==0:
+        return 0
+    elif n==1:
+        return 1
+    else:
+        return Fibonacci(n-1)+Fibonacci(n-2)
+
+print(Fibonacci(7)) # 0 1 1 2 3 5 8 13
---
+++ Örnekler/17.Ornek_fibonacci2.py
@@ -0,0 +1,11 @@
+n=int(input("Adım Sayısı giriniz."))
+
+def Fibonacci(n):
+    if n==0:
+        return 0
+    elif n==1:
+        return 1
+    else:
+        return Fibonacci(n-1)+Fibonacci(n-2)
+
+print(Fibonacci(n))
